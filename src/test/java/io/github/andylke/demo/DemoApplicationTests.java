befa2e16d3147ebe0b4aa121859968e91b9bcf0d
Andy Lian <andylian@silverlakeaxis.com>
2022-02-25 12:16:58 +0800

    Initial commit

---
+++ src/test/java/io/github/andylke/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package io.github.andylke.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void loadContext() {}
+}
